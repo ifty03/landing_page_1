4c41f012431af24c631d2e74cb4ff7af4d25a9e8
ifty03 <ashikulislamb84@gmail.com>
2022-09-16 17:00:53 +0600

    final commit

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -3,7 +3,7 @@ import Review from "./Review";
 
 const Reviews = () => {
   return (
-    <div className="lg:pt-32 md:pt-24 pt-16 pb-5 ">
+    <div className="lg:pt-32 md:pt-24 pt-16 pb-16">
       <p className="text-center text-sm">3940+ Happy Users</p>
       <p className="text-center text-sm">Users</p>
       <h2 className="px-10 text-3xl md:text-4xl text-center font-bold mt-2">
